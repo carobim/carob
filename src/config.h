3cb32c73792d97cfa0a8731abe6b6843f603de04
Paul Merrill <pdm@pdm.me>
2019-08-31 23:32:22 -0700

    Version: v0.5.0

--- src/config.h
+++ src/config.h
@@ -26,7 +26,7 @@
 // **********
 
 /* Release version. */
-#define TSUNAGARI_RELEASE_VERSION "Tsunagari Tile Engine 0.4.1"
+#define TSUNAGARI_RELEASE_VERSION "Tsunagari Tile Engine 0.5.0"
 
 /* Tsunagari config file. */
 #define CLIENT_CONF_PATH "./client.json"
