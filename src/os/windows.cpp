1900ed82090307a5726876866069422d4f8dc731
Paul Merrill <pdm@pdm.me>
2019-09-22 20:13:21 -0700

    os/windows: Enable Windows visual styles

--- src/os/windows.cpp
+++ src/os/windows.cpp
@@ -39,6 +39,11 @@
 #include "util/string.h"
 #include "util/vector.h"
 
+#pragma comment(linker, \
+                "\"/manifestdependency:type='win32' " \
+                "name='Microsoft.Windows.Common-Controls' version='6.0.0.0' " \
+                "processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 extern "C" {
 #define MAX_PATH 260
 
