2596216fec961b5b731af7350f718bea67942513
Paul Merrill <pdm@pdm.me>
2020-12-05 04:26:11 -0800

    function: Fix GCC 8.3 compile warning

--- src/util/function.h
+++ src/util/function.h
@@ -216,8 +216,13 @@ template<class R, class... ArgTypes>
 template<class F>
 Function<R(ArgTypes...) noexcept>::Function(F something) noexcept {
     if (sizeof(function::func<F, R(ArgTypes...) noexcept>) <= sizeof(buf)) {
+        // This warning occurs on GCC 8.3 because of incomplete static branch
+        // analysis.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wplacement-new"
         f = new ((void*)&buf)
                 function::func<F, R(ArgTypes...) noexcept>(static_cast<F&&>(something));
+#pragma GCC diagnostic pop
     }
     else {
         // f = new function::func<F, R(ArgTypes...)>(
