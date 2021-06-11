9503b539f7440e8d6bc88d185f4729b45bdaa5e6
Paul Merrill <pdm@pdm.me>
2021-06-10 22:52:59 -0700

    chrono: Fix timebase for Apple M1

--- src/os/mac-chrono.cpp
+++ src/os/mac-chrono.cpp
@@ -1,7 +1,7 @@
 /*************************************
 ** Tsunagari Tile Engine            **
 ** mac-chrono.cpp                   **
-** Copyright 2019-2020 Paul Merrill **
+** Copyright 2019-2021 Paul Merrill **
 *************************************/
 
 // **********
@@ -29,20 +29,20 @@
 #include "util/assert.h"
 #include "util/int.h"
 
+// mach/arm/kern_return.h
+// mach/i386/kern_return.h
 typedef int kern_return_t;
 
+// mach/mach_time.h
 struct mach_timebase_info {
     uint32_t numer;
     uint32_t denom;
 };
-
 extern "C" {
 uint64_t
 mach_absolute_time() noexcept;
-
 kern_return_t
 mach_timebase_info(mach_timebase_info*) noexcept;
-
 kern_return_t
 mach_wait_until(uint64_t deadline) noexcept;
 }
@@ -51,17 +51,33 @@ mach_wait_until(uint64_t deadline) noexcept;
 
 static struct mach_timebase_info timebase = {0, 0};
 
-Nanoseconds
-chronoNow() noexcept {
+static Nanoseconds
+fromAbsolute(uint64_t machTime) noexcept {
+    uint64_t ns = machTime *
+        static_cast<uint64_t>(timebase.numer) /
+        static_cast<uint64_t>(timebase.denom);
+    return static_cast<Nanoseconds>(ns);
+}
+
+static uint64_t
+toAbsolute(Nanoseconds ns) noexcept {
+    return static_cast<uint64_t>(ns) *
+        static_cast<uint64_t>(timebase.denom) /
+        static_cast<uint64_t>(timebase.numer);
+}
+
+static void
+initTimebase(void) {
     if (timebase.numer == 0 && timebase.denom == 0) {
         kern_return_t err = mach_timebase_info(&timebase);
         assert_(err == KERN_SUCCESS);
     }
+}
 
-    uint64_t machTime = mach_absolute_time();
-    uint64_t ns = machTime * timebase.numer / timebase.denom;
-
-    return static_cast<Nanoseconds>(ns);
+Nanoseconds
+chronoNow(void) noexcept {
+    initTimebase();
+    return fromAbsolute(mach_absolute_time());
 }
 
 void
@@ -70,10 +86,10 @@ chronoSleep(Nanoseconds ns) noexcept {
         return;
     }
 
-    uint64_t deadline = chronoNow() + ns;
+    initTimebase();
+    uint64_t deadline = mach_absolute_time() + toAbsolute(ns);
 
-    kern_return_t err;
-
+    kern_return_t err = KERN_SUCCESS;
     do {
         err = mach_wait_until(deadline);
     } while (err != KERN_SUCCESS);
