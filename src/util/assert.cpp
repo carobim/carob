113fa5449a53b451e64d4602372f7a2b56ebd257
Paul Merrill <pdm@pdm.me>
2021-06-10 20:27:22 -0700

    assert: Detect macOS debuggers

--- src/util/assert.cpp
+++ src/util/assert.cpp
@@ -37,6 +37,80 @@ extern "C" __declspec(dllimport) int __stdcall IsDebuggerPresent() noexcept;
 void __cdecl __debugbreak();  // Cannot be noexcept.
 #    endif
 
+#    ifdef __APPLE__
+// sys/_types/_mach_port_t.h
+typedef uint32_t mach_port_t;
+
+// mach/arm/exception.h
+// mach/i386/exception.h
+#define EXC_TYPES_COUNT 14
+
+// mach/arm/kern_return.h
+// mach/i386/kern_return.h
+typedef int32_t kern_return_t;
+
+// mach/thread_status.h
+typedef int32_t thread_state_flavor_t;
+typedef int32_t* thread_state_flavor_array_t;
+
+// mach/exception_types.h
+typedef uint32_t exception_mask_t;
+typedef int32_t exception_behavior_t;
+typedef exception_behavior_t* exception_behavior_array_t;
+typedef exception_mask_t* exception_mask_array_t;
+typedef thread_state_flavor_t* exception_flavor_array_t;
+#define EXC_BREAKPOINT (1 << 6)
+
+// mach/kern_return.h
+#define KERN_SUCCESS 0
+
+// mach/mach_init.h
+extern mach_port_t mach_task_self_;
+#define mach_task_self() mach_task_self_
+
+// mach/mach_types.h
+typedef mach_port_t task_t;
+typedef mach_port_t exception_handler_t;
+typedef exception_handler_t* exception_handler_array_t;
+
+// mach/message.h
+typedef uint32_t mach_msg_type_number_t;
+
+// mach/port.h
+#define MACH_PORT_VALID(name) ((name) != 0 && (name) != ((uint32_t)~0))
+
+// mach/task.h
+extern "C" kern_return_t
+task_get_exception_ports(task_t, exception_mask_t, exception_mask_array_t,
+                         mach_msg_type_number_t*, exception_handler_array_t,
+                         exception_behavior_array_t, exception_flavor_array_t)
+                        noexcept;
+
+static bool
+isDebuggerPresent(void) {
+    mach_msg_type_number_t count = 0;
+    exception_mask_t masks[EXC_TYPES_COUNT];
+    mach_port_t ports[EXC_TYPES_COUNT];
+    exception_behavior_t behaviors[EXC_TYPES_COUNT];
+    thread_state_flavor_t flavors[EXC_TYPES_COUNT];
+
+    exception_mask_t mask = EXC_BREAKPOINT;
+    kern_return_t result =
+        task_get_exception_ports(mach_task_self(), mask, masks, &count, ports,
+                                 behaviors, flavors);
+    if (result == KERN_SUCCESS) {
+        for (mach_msg_type_number_t portIndex = 0;
+             portIndex < count;
+             portIndex++) {
+            if (MACH_PORT_VALID(ports[portIndex])) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+#    endif
+
 #    ifdef __linux__
 enum ptrace_request {
     PTRACE_TRACEME = 0,
@@ -44,6 +118,17 @@ enum ptrace_request {
 };
 extern "C" long
 ptrace(enum ptrace_request, ...) noexcept;
+
+static bool
+isDebuggerPresent(void) {
+    if (ptrace(PTRACE_TRACEME, 0, 0, 0) == -1) {
+        return true;
+    }
+    else {
+        ptrace(PTRACE_DETACH, 0, 0, 0);
+        return false;
+    }
+}
 #    endif
 
 static bool
@@ -53,15 +138,12 @@ haveDebugger() noexcept {
     if (result == 2) {
 #    if defined(_WIN32)
         result = IsDebuggerPresent();
+#    elif defined(__APPLE__)
+        result = isDebuggerPresent();
 #    elif defined(__linux__)
-        if (ptrace(PTRACE_TRACEME, 0, 0, 0) == -1) {
-            result = true;
-        }
-        else {
-            ptrace(PTRACE_DETACH, 0, 0, 0);
-            result = false;
-        }
+        result = isDebuggerPresent();
 #    else
+#        warn not sure how to check for debugger
         result = false;
 #    endif
     }
@@ -83,6 +165,8 @@ triggerDebugger() noexcept {
     __asm__ __volatile__(".inst 0xd4200000");
 #    elif defined(__arm__)
     __asm__ __volatile__(".inst 0xe7f001f0");
+#    else
+#        warn not sure how to trap for debugger
 #    endif
 }
 
