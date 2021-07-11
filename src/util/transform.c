03c5e8781ad17a3d48b2af8427155ac69430923f
Paul Merrill <pdm@pdm.me>
2021-07-11 00:11:24 -0700

    transform: Fix build on Alpine Linux

--- src/util/transform.c
+++ src/util/transform.c
@@ -29,6 +29,7 @@
 struct Transform
 transformIdentity() {
     struct Transform result;
+
     result.m[0] = 1;
     result.m[1] = 0;
     result.m[2] = 0;
@@ -45,12 +46,14 @@ transformIdentity() {
     result.m[13] = 0;
     result.m[14] = 0;
     result.m[15] = 1;
+
     return result;
 }
 
 struct Transform
 transformScale(float x, float y) {
     struct Transform result;
+
     result.m[0] = x;
     result.m[1] = 0;
     result.m[2] = 0;
@@ -67,12 +70,14 @@ transformScale(float x, float y) {
     result.m[13] = 0;
     result.m[14] = 0;
     result.m[15] = 1;
+
     return result;
 }
 
 struct Transform
 transformTranslate(float x, float y) {
     struct Transform result;
+
     result.m[0] = 1;
     result.m[1] = 0;
     result.m[2] = 0;
@@ -89,17 +94,21 @@ transformTranslate(float x, float y) {
     result.m[13] = y;
     result.m[14] = 0;
     result.m[15] = 1;
+
     return result;
 }
 
 struct Transform
 transformMultiply(struct Transform a, struct Transform b) {
     struct Transform result;
-    for (int i = 0; i < 16; i++) {
+    int i;
+
+    for (i = 0; i < 16; i++) {
         result.m[i] = a.m[(i & ~3)    ] * b.m[(i & 3)     ] +
                       a.m[(i & ~3) | 1] * b.m[(i & 3) |  4] +
                       a.m[(i & ~3) | 2] * b.m[(i & 3) |  8] +
                       a.m[ i       | 3] * b.m[(i & 3) | 12];
     }
+
     return result;
 }
